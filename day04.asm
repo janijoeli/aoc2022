cbf67749d19d6af6c5dcc02abdc5f252036d2d80
Jani Joeli <jani.joeli@kodekompile.com>
2023-01-11 15:53:07 +0200

    Day04 part2

--- day04.asm
+++ day04.asm
@@ -1,19 +1,26 @@
 /**
+	Part 1 rules:
 	If range starts are equal, one of them always fits to the other.
 	If range1_start < range2_start, range1_end must be >= range2_end for 2 to fit 1.
 	If range1_start > range2_start, range2_end must be >= range1_end for 1 to fit 2.
+	Part 2 rules:
+	If range1 ends before r2 starts, or if range1 starts after r2 ends, they don't overlap.
+	
+	These two rulesets could be combined, i.e. test for any overlap first and then do less 
+	tests to see if ranges completely overlap, but hey ho, on to the next one.
 */
 
-.const total		= $fb			// 16bit, $0000
-.const input_ptr	= $39			// pointer to input data table
-.const ones			= $2			// temporary holder for a ones digit
+.const fits_count		= $fb		// 16bit, $0000
+.const overlaps_count	= $fd		// 16bit, $0000
+.const input_ptr		= $39		// pointer to input data table
+.const ones				= $2		// temporary holder for a ones digit
 
-.const range1_start	= $3			// Parsed range start and end points
-.const range1_end	= $4
-.const range2_start	= $5
-.const range2_end	= $6
+.const range1_start		= $3		// Parsed range start and end points
+.const range1_end		= $4
+.const range2_start		= $5
+.const range2_end		= $6
 
-.const LF			= $0a			// ASCII code for Line Feed
+.const LF				= $0a		// ASCII code for Line Feed
 
 * = $0801 "Basic Header"
 				.word start, input; .byte $9e; .text "2061"; .byte 0,0,0
@@ -39,22 +46,31 @@ start:			ldy #0				// Read two ranges from a line
 				// Part1 - testing for fully overlapping ranges
 compare_ranges:	lda range1_start
 				cmp range2_start
-				beq fits				// Range starts are equal, one fits another
-				bcc is_r1_longer		// r1 starts earlier, does it also end later?
-				lda range2_end			// r1 starts later, does r2 end later?
+				beq fits			// Range starts are equal, one fits another
+				bcc is_r1_longer	// r1 starts earlier, does it also end later?
+				lda range2_end		// r1 starts later, does r2 end later?
 				cmp range1_end
-				bcs fits				// r2 ends at or after r1 end, fits
-				bcc start				// r2 ends before r1, no fit
+				bcs fits			// r2 ends at or after r1 end, fits
+				bcc check_overlap	// r2 ends before r1, no fit
 is_r1_longer:	lda range1_end
 				cmp range2_end
-				bcc start				// r1 ends before r2, no fit
-
-		fits:	inc total
+				bcc check_overlap	// r1 ends before r2, no fit
+		fits:	inc fits_count
+				bne check_overlap
+				inc fits_count+1
+				bne check_overlap
+				
+				// Part2 - testing for partially overlapping ranges
+check_overlap:	lda range1_end
+				cmp range2_start
+				bcc start			// r1 ends before r2 starts, r1 is fully below r2, no overlap
+				lda range2_end
+				cmp range1_start
+				bcc start			// r2 ends before r1 starts, r1 is fully above r2, no overlap
+				inc overlaps_count	// r1 is neither above or below r2, thus the ranges must overlap
 				bne start
-				inc total+1
+				inc overlaps_count+1
 				bne start
-
-
 				
 read_number:	lax (input_ptr),y	// Read 1st digit (either ones or tens)
 				beq finish			// If 0, jump to displaying the result
@@ -76,14 +92,18 @@ read_number:	lax (input_ptr),y	// Read 1st digit (either ones or tens)
 	one_digit:	rts					// X holds the complete number
 				
 				
-finish:			ldx total			// print total as decimal number
-				lda total+1
+finish:			ldx fits_count			// print fits_count as decimal number
+				lda fits_count+1
+				jsr $bdcd
+				jsr $aad7				// New line
+				ldx overlaps_count		// print overlaps_count as decimal number
+				lda overlaps_count+1
 				jsr $bdcd
 				jmp *
 				
 * = * "Input Data"
 input:			.import binary "input/day04_input.txt"
-				// .import binary "input/day04_input_test.txt"	// Expected: 2
-				// .import binary "input/day04_input_test_better.txt"	// Expected: 5
+				// .import binary "input/day04_input_test.txt"	// Expected: part1 = 2, part2 = 4
+				// .import binary "input/day04_input_test_better.txt"	// Expected: part1 = 5, part2 = 7
 				.byte 0 // End of table
 * = * "End"
